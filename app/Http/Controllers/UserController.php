8fec3232f738df9e445d04c8583b157484cfbc73
putricha_ <putrinorchasana@gmail.com>
2024-03-17 06:04:08 +0700

    retrieving single models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,15 +26,20 @@ class UserController extends Controller
         // $user = UserModel::all();
         // return view('user', ['data' => $user]);
 
-        $data = [
-            'level_id'  => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
+        // $data = [
+        //     'level_id'  => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
         
-        UserModel::create($data);
-        $user = UserModel::all();
+        // UserModel::create($data);
+
+        // $user = UserModel::firstwhere('level_id',1);
+
+        $user = UserModel::findOr(20,['username','nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
 
     }
