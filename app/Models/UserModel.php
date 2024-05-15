e63bf98be565d53b12aab5cbf33a68af434f65b4
putricha_ <putrinorchasana@gmail.com>
2024-05-15 13:59:15 +0700

    [jobsheet 11] Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -23,7 +23,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -52,10 +52,17 @@ class UserModel extends Authenticatable implements JWTSubject
         'username',
         'nama',
         'password',
+        'image'
     ];
 
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    protected  function image():Attribute{
+        return Attribute::make(
+            get:fn($image)=>url('/storage/posts/'.$image),
+        );
     }
 }
